7f0b64c8a42e43bfc2ec50a0506256626103fe4d
KieranVieira <kieranvieira@live.com>
2019-03-11 12:02:56 -0400

    Created knex table for users

--- knexfile.js
+++ knexfile.js
@@ -5,24 +5,16 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+      filename: './database/github.db3'
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+      directory: "./database/migrations"
+    },
+    seeds: {
+      directory: "./database/seeds"
+    },
+    useNullAsDefault: true,
   },
 
   production: {
